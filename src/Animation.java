ddac7893e90f372d23b1f736e4d950d4f70648e3
andreahassler <andrea.hassler@students.fhnw.ch>
2016-09-26 16:17:32 +0200

    Animation grösser werdender Kreis

---
+++ src/Animation.java
@@ -0,0 +1,97 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Animation extends JPanel implements ActionListener {
+
+    private Ellipse2D.Float ellipse = new Ellipse2D.Float();
+    private double esize;
+    private double maxSize = 0;
+    private boolean initialize = true;
+    Timer timer;
+    ActionListener updateProBar;
+    private String[][] data;
+    private Data test;
+    private Patient patient;
+
+    public Animation() {
+        setXY( Math.random(), 200, 200);
+        timer = new Timer(40, this); //"50" setzt Geschwindigkeit der Animation, wie schnell wird neugezeichnet
+        timer.setInitialDelay(190);
+        timer.start();
+    }
+
+    public Animation(Patient patient){ //Konstruktor für Patient Klasse
+        this.patient=patient;
+    }
+    public Patient getPatient(){
+        return patient;
+    }
+
+
+    public void setXY(double size, int w, int h) {
+        esize = size;
+        ellipse.setFrame(50, 50, size, size); //Position der Animation
+
+    }
+
+    public void getXY(Patient patient){
+        patient.getPatientID(test);
+        System.out.println(test.getLineCounter());
+    }
+
+    public void reset(int w, int h) { //methode reset wird im drawing genutzt
+        maxSize = w / 4;
+        setXY(maxSize * Math.random(), w, h);
+    }
+    public void step(int w, int h) { //methode step wird im drawing genutzt
+        esize++;
+        if (esize > maxSize) {
+            setXY(1, w, h);
+        } else {
+            ellipse.setFrame(ellipse.getX(), ellipse.getY(), esize, esize);
+        }
+    }
+
+
+
+    public void render(int w, int h, Graphics2D g2) {
+        g2.setColor(Color.GREEN);
+        g2.draw(ellipse);
+    }
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2.setRenderingHints(rh);
+        Dimension size = getSize();
+        if (initialize) {
+            reset(size.width, size.height);
+            initialize = false;
+        }
+        this.step(size.width, size.height);
+        render(size.width, size.height, g2);
+    }
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("TimerBasedAnimation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Animation());
+        frame.setSize(450, 350);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
