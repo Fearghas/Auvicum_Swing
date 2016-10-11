abc6d40c6eae3b1f6d534226bcfcf8d81d83d2d2
Briareus <bryan.freiermuth@students.fhnw.ch>
2016-10-11 18:26:20 +0200

    Missing link is Timer

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.ParseException;
 
 /**
  * Created by Andreas on 15.06.2016.
@@ -6,13 +7,15 @@ import java.io.*;
 
 public class Node
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, ParseException
     {
         MainFrame mainFrame = new MainFrame();
         mainFrame.createMainFrame();
         mainFrame.waitingForUser(); //Barriere => warten bis File ausgesucht wurde, dann gehts weiter mit Anweisungen unten
         Data csvFile = new Data(mainFrame.getCsvFile());
         mainFrame.setList(csvFile.storeContent());
-
+        //System.out.println(csvFile.storeContent()[1][1].toString());
+        Logic test = new Logic(csvFile.storeContent());
+        test.useLogic();
     }
 }
