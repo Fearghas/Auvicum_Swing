b3b7945680ff39f1f046e8497e2faa0365ca1c7b
andreahassler <andrea.hassler@students.fhnw.ch>
2016-10-21 14:11:07 +0200

    Dropdown menü für filter mit actionlistener

--- src/Animation.java
+++ src/Animation.java
@@ -6,49 +6,52 @@ import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Animation extends JPanel implements ActionListener {
 
     private Ellipse2D.Float ellipse = new Ellipse2D.Float();
-    private double esize;
+    private Ellipse2D.Float ellipse1 = new Ellipse2D.Float();
+    private Ellipse2D.Float ellipse2 = new Ellipse2D.Float();
+    private Ellipse2D.Float ellipse3 = new Ellipse2D.Float();
+    private Ellipse2D.Float ellipse4 = new Ellipse2D.Float();
+    private Ellipse2D.Float ellipse5 = new Ellipse2D.Float();
+    private double ellipseSize;
     private double maxSize = 0;
     private boolean initialize = true;
     private Timer timer;
     private String[][] list;
 
 
-
     public Animation(String[][] list)
     {
         this.list = list;
-        //System.out.println(list.length);
-       /*int counter = 0;
-        for (int i = 0; i < list.length; i++)
-        {
-            if (list[i][1].contains("2016"))
-            {
-                counter++;
-            }
-        }
-        System.out.println(counter);*/
-        double random = Math.random();
-        System.out.println("Random: " + random);
-        setXY( random, 200, 200);//Math.random = size => ersetzen und Zugriff auf Data erstellen!
-        //setXY(counter, 200, 200);
+        System.out.println(list.length);
+        setXY( Math.random(), 200, 200);//Math.random = size => ersetzen und Zugriff auf Data erstellen!
         timer = new Timer(40, this); //"50" setzt Geschwindigkeit der Animation, wie schnell wird neugezeichnet
         timer.setInitialDelay(190);
         timer.start();
     }
 
+    public Timer getTimer() {
+        return this.timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
 
     public void setXY(double size, int w, int h) //Ellipsen Grösse und Anfangsposition bestimmen
     {
-        System.out.println("Size in setXY Methode: " + size);
-        esize = size;
-        ellipse.setFrame(50, 50, size, size); //Frame anhand der Anfangsgrösse setzen?
+        ellipseSize = size;
+        ellipse.setFrame(Math.random()*150, Math.random()*250, 250, size); //Position der Animation 50=x, 50=y, Grösse der elipse height = size, width = size
+        ellipse1.setFrame(25,250,size,size);
+        ellipse2.setFrame(10,10,size,size);
+        ellipse3.setFrame(160,60,size,size);
+        ellipse4.setFrame(155,55,size,size);
+        ellipse5.setFrame(200,30,size,size);
+
 
     }
 
@@ -57,17 +60,31 @@ public class Animation extends JPanel implements ActionListener {
         setXY(maxSize * Math.random(), w, h);
     }
     public void step(int w, int h) { //methode step wird im drawing genutzt
-        esize++;
-        if (esize > maxSize) {
+        ellipseSize++;
+        if (ellipseSize > maxSize) {
             setXY(1, w, h);
         } else {
-            ellipse.setFrame(ellipse.getX(), ellipse.getY(), esize, esize);
+            ellipse.setFrame(ellipse.getX(), ellipse.getY(), ellipseSize, ellipseSize);//Sets the location and size of the framing rectangle of this Shape to the specified rectangular values.
+            ellipse1.setFrame(ellipse1.getX(), ellipse1.getY(), ellipseSize, ellipseSize);
+            ellipse2.setFrame(ellipse2.getX(), ellipse2.getY(), ellipseSize, ellipseSize);
+            ellipse3.setFrame(ellipse3.getX(), ellipse3.getY(), ellipseSize, ellipseSize);
+            ellipse4.setFrame(ellipse4.getX(), ellipse4.getY(), ellipseSize, ellipseSize);
+            ellipse5.setFrame(ellipse5.getX(), ellipse5.getY(), ellipseSize, ellipseSize);
+
         }
     }
 
     public void render(int w, int h, Graphics2D g2) {
         g2.setColor(Color.GREEN);
         g2.draw(ellipse);
+        g2.draw(ellipse1);
+        g2.draw(ellipse2);
+        g2.draw(ellipse3);
+        g2.draw(ellipse4);
+        g2.draw(ellipse5);
+        g2.fill(ellipse);
+        g2.fill(ellipse1);
+
     }
     public void paint(Graphics g) {
         super.paintComponent(g);
@@ -84,18 +101,18 @@ public class Animation extends JPanel implements ActionListener {
         this.step(size.width, size.height);
         render(size.width, size.height, g2);
     }
+
     public void actionPerformed(ActionEvent e) {
         repaint();
     }
 
+    public void stopTimer(){
+        timer.stop();
+    }
 
-    /*public static void main(String[] args) {
+    public void startTimer(){
+        timer.restart();
+    }
 
-        JFrame frame = new JFrame("TimerBasedAnimation");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new Animation());
-        frame.setSize(450, 350);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }*/
+
 }
